750617349d72dbd5ae13ecc64341f89d34f8865a
vominhduong <vominhduong2005@gmail.com>
2026-04-24 14:16:23 +0700

    k8s-config

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const apiBaseUrl = (import.meta.env.VITE_API_BASE_URL || "").replace(/\/$/, "");
+
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: apiBaseUrl,
 });
 
 api.interceptors.request.use((config) => {
